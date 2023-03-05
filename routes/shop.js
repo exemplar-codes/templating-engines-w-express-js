6e6c2822920f943148d151f9dbd59b1103a05276
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-06 00:10:35 +0530

    Demo placeholder in Pug

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   console.log('shop.js', adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop');
+  res.render("shop", { prods: adminData.products, docTitle: "My shop" });
 });
 
 module.exports = router;
