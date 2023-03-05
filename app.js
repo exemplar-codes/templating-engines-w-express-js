2ec3459c867b46cb703b5daf2b0d4f6c8f8013dc
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-05 23:52:57 +0530

    Install and use pug

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.set('view engine', 'pug');
+app.set('views', 'views'); // not needed for this case, actually
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
