a3bfc87a5bfa29341dddcf8591b9c4867899e3d5
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-03-08 18:01:03 +0530

    Add admin page

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,10 @@ const products = [{title: "Great book"}];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { myActivePath: 'on-admin-page' });
+  res.render('add-product', {
+    myActivePath: 'on-admin-page',
+    docTitle: 'Add product',
+  });
 });
 
 // /admin/add-product => POST
